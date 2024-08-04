4f11ab6bb91efdfc676fab46a54a7f82d5e4056e
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-04 13:23:15 +0300

    .

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -5,7 +5,6 @@ from pytest_django.asserts import assertFormError, assertRedirects
 
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment
-from news.pytest_tests import UrlConst
 
 FORM_DATA = {
     'text': 'Новый текст',
