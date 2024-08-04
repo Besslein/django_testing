65a5eef33d6186ccc55208171def9ddd0ee2e47b
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-04 09:28:31 +0300

    .

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -70,34 +70,3 @@ def news_list():
         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
     ]
     News.objects.bulk_create(all_news)
-
-
-@pytest.fixture
-def url_to_comments(news):
-    news_url = reverse('news:detail', args=(news.id,))
-    return news_url + '#comments'
-
-
-@pytest.fixture
-def edit_url(comment):
-    return reverse('news:edit', args=(comment.id,))
-
-
-@pytest.fixture
-def delete_url(comment):
-    return reverse('news:delete', args=(comment.id,))
-
-
-@pytest.fixture
-def news_detail_url(news):
-    return reverse('news:detail', args=(news.id,))
-
-
-@pytest.fixture
-def news_home_url():
-    return reverse('news:home')
-
-
-@pytest.fixture
-def login_url():
-    return reverse('users:login')
