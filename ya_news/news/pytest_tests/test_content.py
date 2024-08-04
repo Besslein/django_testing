9a85c79ad474f1821991c400864bdaf1f4cd569c
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-04 13:44:52 +0300

    .

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -2,7 +2,17 @@ from django.conf import settings
 
 from news.forms import CommentForm
 
+
 FORM_NAME = 'form'
+
+
+class Const():
+    URL_TO_COMMENTS = 'news:detail'
+    EDIT_URL = 'news:edit'
+    DELETE_URL = 'news:delete'
+    NEWS_DETAIL_URL = 'news:detail'
+    NEWS_HOME_URL = 'news:home'
+    LOGIN_URL = 'users:login'
 
 
 def test_authorized_client_has_form(author_client, news_detail_url):
