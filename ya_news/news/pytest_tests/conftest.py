d04bf1bd374a07314123cab27a4497df394fc0cc
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-04 12:36:58 +0300

    .

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 import pytest
 from django.conf import settings
 from django.utils import timezone
-from django.urls import reverse
 
 from news.models import Comment, News
 
@@ -16,9 +15,8 @@ def author(django_user_model):
 
 @pytest.fixture
 def author_client(author, client):
-    users_login = reverse('users:login')
     client.force_login(author)
-    return users_login, client
+    return client
 
 
 @pytest.fixture
@@ -34,16 +32,12 @@ def reader_client(reader, client):
 
 @pytest.fixture
 def news():
-    news_1 = reverse('news:home')
-    news_2 = reverse('news:detail', args=(news.id,))
-    return news_1, news_2, News.objects.create(title='Заголовок', text='Текст')
+    return News.objects.create(title='Заголовок', text='Текст')
 
 
 @pytest.fixture
 def comment(news, author):
-    edit_url = reverse('news:edit', args=(comment.id,))
-    delete_url = reverse('news:delete', args=(comment.id,))
-    return delete_url, edit_url, Comment.objects.create(
+    return Comment.objects.create(
         news=news,
         author=author,
         text='Текст комментария'
@@ -65,7 +59,6 @@ def comments(news, author):
 
 @pytest.fixture
 def news_list():
-    news_detail = reverse('news:detail', args=(news.id,))
     today = timezone.now()
     all_news = [
         News(
@@ -76,4 +69,3 @@ def news_list():
         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
     ]
     News.objects.bulk_create(all_news)
-    return news_detail
