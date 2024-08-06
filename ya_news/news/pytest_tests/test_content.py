0fb70c432b8a51d1db87dcded97f32cb107c62de
Besslein <stielhadgranate24@gmail.com>
2024-08-06 13:32:08 +0000

    .

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -7,18 +7,18 @@ from .conftest import NEWS_DETAIL_URL, NEWS_HOME_URL
 FORM_NAME = 'form'
 
 
-def test_authorized_client_has_form(author_client, news_detail_url):
+def test_authorized_client_has_form(author_client):
     response = author_client.get(NEWS_DETAIL_URL)
     assert FORM_NAME in response.context
     assert isinstance(response.context[FORM_NAME], CommentForm)
 
 
-def test_anonymous_client_has_no_form(client, news_detail_url):
+def test_anonymous_client_has_no_form(client):
     response = client.get(NEWS_DETAIL_URL)
     assert FORM_NAME not in response.context
 
 
-def test_comments_order(client, news_detail_url, comments):
+def test_comments_order(client, comments):
     response = client.get(NEWS_DETAIL_URL)
     all_comments = response.context['news'].comment_set.all()
     all_created = [comment.created for comment in all_comments]
