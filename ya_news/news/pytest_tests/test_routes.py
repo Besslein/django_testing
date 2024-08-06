4e693527870457b3d515a71a3467f6819cbafa6e
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-06 23:46:48 +0300

    .

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -56,11 +56,11 @@ def test_pages_availability_for_different_users(
 @pytest.mark.parametrize(
     'name, news_object',
     (
-        (EDIT_URL, pytest.lazy_fixture('news')),
-        (DELETE_URL, pytest.lazy_fixture('news')),
+        (EDIT_URL),
+        (DELETE_URL),
     ),
 )
-def test_redirects(client, name, news_object, login_url):
+def test_redirects(client, name, news_object):
     expected_url = f'{LOGIN_URL}?next={name}'
     response = client.get(name, args=(news_object.id,))
     assertRedirects(response, expected_url)
