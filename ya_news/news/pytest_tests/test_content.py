09feea611c1cfc9c0da3289aa03e3b3e92e94657
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-06 20:09:59 +0300

    .

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,3 +1,4 @@
+import pytest
 from django.conf import settings
 
 from news.forms import CommentForm
@@ -7,17 +8,20 @@ from .conftest import UrlConst
 FORM_NAME = 'form'
 
 
+@pytest.mark.django_db
 def test_authorized_client_has_form(author_client, news_detail_url):
     response = author_client.get(UrlConst.NEWS_DETAIL_URL)
     assert FORM_NAME in response.context
     assert isinstance(response.context[FORM_NAME], CommentForm)
 
 
+@pytest.mark.django_db
 def test_anonymous_client_has_no_form(client, news_detail_url):
     response = client.get(UrlConst.NEWS_DETAIL_URL)
     assert FORM_NAME not in response.context
 
 
+@pytest.mark.django_db
 def test_comments_order(client, news_detail_url, comments):
     response = client.get(UrlConst.NEWS_DETAIL_URL)
     all_comments = response.context['news'].comment_set.all()
@@ -26,6 +30,7 @@ def test_comments_order(client, news_detail_url, comments):
     assert all_created == sorted_created
 
 
+@pytest.mark.django_db
 def test_news_count(client, news_home_url, news_list):
     response = client.get(UrlConst.NEWS_HOME_URL)
     assert 'object_list' in response.context
@@ -33,6 +38,7 @@ def test_news_count(client, news_home_url, news_list):
     assert object_list.count() == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
+@pytest.mark.django_db
 def test_news_order(client, news_home_url, news_list):
     response = client.get(UrlConst.NEWS_HOME_URL)
     assert 'object_list' in response.context
