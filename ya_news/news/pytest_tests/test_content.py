4d838f67edb83b85216d313bec2c634772f2dc75
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-06 21:33:03 +0300

    .

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -7,10 +7,10 @@ from .conftest import NEWS_DETAIL_URL, NEWS_HOME_URL
 FORM_NAME = 'form'
 
 
-def test_authorized_client_has_form(author_client, news):
-    response = author_client.get(NEWS_DETAIL_URL, args=(news.id,))
+def test_authorized_client_has_form(author_client):
+    response = author_client.get(NEWS_DETAIL_URL)
     assert FORM_NAME in response.context
-    assert isinstance(response.context[FORM_NAME], CommentForm)
+    assert isinstance(response.context.get('form'), CommentForm)
 
 
 def test_anonymous_client_has_no_form(client, news):
@@ -18,8 +18,8 @@ def test_anonymous_client_has_no_form(client, news):
     assert FORM_NAME not in response.context
 
 
-def test_comments_order(client, comments, news):
-    response = client.get(NEWS_DETAIL_URL, args=(news.id,))
+def test_comments_order(client, comments):
+    response = client.get(NEWS_DETAIL_URL)
     all_comments = response.context['news'].comment_set.all()
     all_created = [comment.created for comment in all_comments]
     sorted_created = sorted(all_created)
@@ -28,15 +28,13 @@ def test_comments_order(client, comments, news):
 
 def test_news_count(client):
     response = client.get(NEWS_HOME_URL)
-    assert 'object_list' in response.context
-    object_list = response.context['object_list']
+    object_list = response.context['object_list'].count()
     assert object_list.count() == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
 def test_news_order(client):
     response = client.get(NEWS_HOME_URL)
-    assert 'object_list' in response.context
-    object_list = response.context['object_list']
+    object_list = response.context['object_list'].count()
     all_dates = [news.date for news in object_list]
     sorted_dates = sorted(all_dates, reverse=True)
     assert all_dates == sorted_dates
