87e75d9e5ab00740fb5c8623358190e9deea07f0
Andrey Efremenkov <Klontarf@yandex.ru>
2024-08-08 01:19:12 +0300

    .

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertRedirects
-from .conftest import DELETE_URL, EDIT_URL, LOGIN_URL
+from .conftest import DELETE_URL, EDIT_URL
 
 
 HOME_URL = reverse('news:home')
